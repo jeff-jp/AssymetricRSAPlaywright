7369d7f8d4490a3ca1c13d1ff2da9a00ffe3981e
Jeff de Paula <jeff-jp@hotmail.com>
2024-09-09 01:57:07 -0300

    Add files via upload

---
+++ AssymetricRSACript/Login.aspx.cs
@@ -0,0 +1,57 @@
+﻿using AssymetricRSACriptPlaywrigth.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AssymetricRSACriptPlaywrigth
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static object Initialization()
+        {
+            string name = "Eu";
+
+            string _urlGerador = HttpContext.Current.Request.Url.Authority + "/Generator.html";
+
+            DadosRSA Keys = Utils.Utils.GerarKeysRSA(_urlGerador);
+
+            var result = new
+            {
+                message = "Teste",
+                nome = name,
+                publicKey =Keys.PublicKey
+            };
+
+            return result;
+        }
+
+
+        [WebMethod]
+        public static object Autenticar(string user, string pass, string diretorio)
+        {
+            string _urlGerador = HttpContext.Current.Request.Url.Authority + "/Generator.html";
+
+            string usuario = Utils.Utils.DecryptRSA(user, _urlGerador);
+            string senha = Utils.Utils.DecryptRSA(pass, _urlGerador);
+
+
+            var result = new
+            {
+                message = "Sucesso"
+            };
+
+            return result;
+        }
+
+    }
+}
